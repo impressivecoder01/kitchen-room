fbb41e40989edf252873891309fd16c53ff01b0d
impressivecoder1 <rohanrahman251@gmail.com>
2026-02-16 22:21:11 +0600

    orderContainer.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Logo from './components/Logo'
 import Navbar from './components/Navbar'
 import Heading from './components/Heading'
 import States from './components/States'
-
+import OrderContainer from './components/OrderContainer'
+import { Suspense } from 'react'
+const loadOrder =()=> fetch('/json/orders.json').then(res => res.json())
 function App() {
-  
-
+  const ordersPromise = loadOrder()
+// console.log(ordersPromise)
   return (
     <>
     <header className='w-11/12 mx-auto py-5'>
@@ -19,9 +21,11 @@ function App() {
       <Heading><h1>Kitchen Room</h1></Heading>
     </section>
     <section>
-      <States></States>
+      <Suspense fallback={`Loading...`}>
+      <OrderContainer promise ={ordersPromise}></OrderContainer>
+      </Suspense>
     </section>
-      {/* <Logo></Logo> */}
+      
     </>
   )
 }
