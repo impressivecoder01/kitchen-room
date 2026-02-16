7ef4fc354712413e724a3cc7bb307f52ae8db01f
impressivecoder1 <rohanrahman251@gmail.com>
2026-02-17 01:09:53 +0600

    cards added

--- src/components/OrderContainer.jsx
+++ src/components/OrderContainer.jsx
@@ -1,12 +1,29 @@
 import React, { use } from 'react';
 import States from './States';
+import Cards from './Cards/Cards';
 
-const OrderContainer = ({promise}) => {
+const OrderContainer = ({ promise }) => {
     const orders = use(promise)
     console.log(orders)
     return (
         <div className=''>
-            <States></States>
+
+            <States order={orders}></States>
+            <section className='w-11/12 mx-auto py-10 grid grid-cols-1 lg:grid-cols-12'>
+                <div className=' lg:col-span-7'>
+                    <h1 className='font-bold text-4xl'>Current Order</h1>
+                    <div className='space-y-5'>
+                        {
+                            orders.map(item => <Cards key={item.id} order= {item}></Cards>)
+                        }
+                    </div>
+                </div>
+                <div className=' lg:col-span-5'>
+                    cooking
+                </div>
+            </section>
+
+
         </div>
     );
 };
--- src/components/States.jsx
+++ src/components/States.jsx
@@ -1,7 +1,7 @@
 import { CookingPot, ScrollText, ShoppingBasket } from 'lucide-react';
 import React from 'react';
 
-const States = () => {
+const States = ({order}) => {
     return (
         <div className='w-11/12 mx-auto py-10 grid grid-cols-1 lg:grid-cols-3 gap-2'>
             {/* total order */}
@@ -9,8 +9,8 @@ const States = () => {
                 <div className="flex flex-col lg:flex-row items-center justify-between">
                     <ScrollText className="animate-pulse" color="#fcb700" size={100} />
                     <div className="text-xl text-center">
-                        Current Orders
-                        <h2 className="text-6xl font-bold">{0}</h2>
+                        Current Orders 
+                        <h2 className="text-6xl font-bold">{order.length}</h2>
                     </div>
                 </div>
             </div>
